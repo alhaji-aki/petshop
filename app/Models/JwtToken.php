db487668b1b83ba528c7f1cf5e864fbcc8245925
Alhaji Aki <abdulkudus2922@gmail.com>
2023-08-13 05:58:49 +0000

    added JwtToken model and migration

---
+++ app/Models/JwtToken.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class JwtToken extends Model
+{
+    /**
+     * The attributes that are not mass assignable.
+     *
+     * @var array<int, string>
+     *
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.PropertyTypeHint
+     */
+    protected $guarded = [
+        'id',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     *
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.PropertyTypeHint
+     */
+    protected $casts = [
+        'restrictions' => 'array',
+        'permissions' => 'array',
+        'expires_at' => 'datetime',
+        'last_used_at' => 'datetime',
+        'refreshed_at' => 'datetime',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2023_08_13_054941_create_jwt_tokens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jwt_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('unique_id');
+            $table->string('token_title');
+            $table->json('restrictions')->default(new Expression('(JSON_ARRAY())'));
+            $table->json('permissions')->default(new Expression('(JSON_ARRAY())'));
+            $table->timestamps();
+            $table->timestamp('expires_at')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('refreshed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jwt_tokens');
+    }
+};

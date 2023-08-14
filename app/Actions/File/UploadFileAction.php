440af8f96bad245a6bee187a4732db3d5160fe1b
Alhaji Aki <abdulkudus2922@gmail.com>
2023-08-14 20:24:55 +0000

    fix phpstan issues

--- app/Actions/File/UploadFileAction.php
+++ app/Actions/File/UploadFileAction.php
@@ -8,33 +8,34 @@ use Illuminate\Support\Str;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Filesystem\FilesystemAdapter;
 
 class UploadFileAction
 {
     public function execute(Model $model, UploadedFile $uploadedFile): File
     {
         $file = new File();
-        $file->model()->associate($model);
         $file->name = Str::random(8);
         $file->type = $uploadedFile->getClientMimeType();
         $file->path = $this->saveFile($file->name, $uploadedFile);
         $file->size = Storage::disk('public')->size($file->path);
 
-        if ($file->path === false) {
-            throw new Exception('Failed to upload file.');
-        }
-
+        $file->model()->associate($model);
         $file->save();
 
         return $file;
     }
 
-    private function saveFile(string $name, UploadedFile $uploadedFile): string|false
+    private function saveFile(string $name, UploadedFile $uploadedFile): string
     {
         $basepath = Str::uuid();
         $filename = "{$name}.{$uploadedFile->getClientOriginalExtension()}";
 
-        return Storage::disk('public')->putFileAs($basepath, $uploadedFile, $filename);
+        $path = Storage::disk('public')->putFileAs($basepath, $uploadedFile, $filename);
+
+        if ($path === false) {
+            throw new Exception('Failed to upload file.');
+        }
+
+        return $path;
     }
 }

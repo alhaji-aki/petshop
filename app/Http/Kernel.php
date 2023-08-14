76284884a3e40f4d6ac57ed4f17470c0fc1a9b5d
Alhaji Aki <abdulkudus2922@gmail.com>
2023-08-14 16:40:08 +0000

    forcing json response

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,6 +43,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \App\Http\Middleware\EnsureAcceptJsonRequest::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],

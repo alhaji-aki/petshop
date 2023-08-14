fbefba05d73e5d8e8b19e620c5567412e96928de
Alhaji Aki <abdulkudus2922@gmail.com>
2023-08-14 20:14:29 +0000

    completed payment management

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable

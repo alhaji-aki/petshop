9941cad41c6180c97a6dd05983c657df70d134b2
Alhaji Aki <abdulkudus2922@gmail.com>
2023-08-15 15:00:46 +0000

    fix php insights issue regarding long function

--- app/Http/Requests/Payment/StorePaymentRequest.php
+++ app/Http/Requests/Payment/StorePaymentRequest.php
@@ -28,48 +28,22 @@ class StorePaymentRequest extends FormRequest
      */
     public function rules(): array
     {
+        $cashOnDelivery = PaymentTypeEnum::CashOnDelivery->value;
+        $bankTransfer = PaymentTypeEnum::BankTransfer->value;
+        $creditCard = PaymentTypeEnum::CreditCard->value;
+
         return [
             'type' => ['required', new Enum(PaymentTypeEnum::class)],
-            "details.first_name" => [
-                'exclude_unless:type,' . PaymentTypeEnum::CashOnDelivery->value,
-                'required', 'string', 'max:255',
-            ],
-            "details.last_name" => [
-                'exclude_unless:type,' . PaymentTypeEnum::CashOnDelivery->value,
-                'required', 'string', 'max:255',
-            ],
-            "details.address" => [
-                'exclude_unless:type,' . PaymentTypeEnum::CashOnDelivery->value,
-                'required', 'string', 'max:255',
-            ],
-            "details.swift" => [
-                'exclude_unless:type,' . PaymentTypeEnum::BankTransfer->value,
-                'required', 'string', 'max:255',
-            ],
-            "details.iban" => [
-                'exclude_unless:type,' . PaymentTypeEnum::BankTransfer->value,
-                'required', 'string', 'max:255',
-            ],
-            "details.name" => [
-                'exclude_unless:type,' . PaymentTypeEnum::BankTransfer->value,
-                'required', 'string', 'max:255',
-            ],
-            "details.holder_name" => [
-                'exclude_unless:type,' . PaymentTypeEnum::CreditCard->value,
-                'required', 'string', 'max:255',
-            ],
-            "details.number" => [
-                'exclude_unless:type,' . PaymentTypeEnum::CreditCard->value,
-                'required', 'string', 'max:255',
-            ],
-            "details.ccv" => [
-                'exclude_unless:type,' . PaymentTypeEnum::CreditCard->value,
-                'required', 'digits:3', 'max:255',
-            ],
-            "details.expire_date" => [
-                'exclude_unless:type,' . PaymentTypeEnum::CreditCard->value,
-                'required', 'date_format:m/y', 'after:now',
-            ],
+            "details.first_name" => ['exclude_unless:type,' . $cashOnDelivery, 'required', 'string', 'max:255'],
+            "details.last_name" => ['exclude_unless:type,' . $cashOnDelivery, 'required', 'string', 'max:255'],
+            "details.address" => ['exclude_unless:type,' . $cashOnDelivery, 'required', 'string', 'max:255'],
+            "details.swift" => ['exclude_unless:type,' . $bankTransfer, 'required', 'string', 'max:255'],
+            "details.iban" => ['exclude_unless:type,' . $bankTransfer, 'required', 'string', 'max:255'],
+            "details.name" => ['exclude_unless:type,' . $bankTransfer, 'required', 'string', 'max:255'],
+            "details.holder_name" => ['exclude_unless:type,' . $creditCard, 'required', 'string', 'max:255'],
+            "details.number" => ['exclude_unless:type,' . $creditCard, 'required', 'string', 'max:255'],
+            "details.ccv" => ['exclude_unless:type,' . $creditCard, 'required', 'digits:3', 'max:255'],
+            "details.expire_date" => ['exclude_unless:type,' . $creditCard, 'required', 'date_format:m/y', 'after:now'],
         ];
     }
 
